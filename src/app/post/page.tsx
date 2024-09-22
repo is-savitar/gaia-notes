874947e97e536a42ba64de4f8ec073a64e3b46f7
is-savitar <savitar746233@gmail.com>
2024-09-22 15:31:39 +0100

    feat: implementing post for page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,11 @@
-import BasicEditor from "@/_components/post/editor";
+import { PlateEditor } from "@/_components/post/plate-editor";
 
 const PostBlog = () => {
-  return <BasicEditor />;
+  return (
+    <main className="max-w-[1200px] mx-auto px-5 sm:px-4">
+      <PlateEditor />
+    </main>
+  );
 };
 
 export default PostBlog;
